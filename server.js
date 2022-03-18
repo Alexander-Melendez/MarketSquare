bae1678045884f62e50b38e0038e64de77be55cb
Sydroux <59854875+Sydroux@users.noreply.github.com>
2022-03-18 12:55:14 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const path = require('path');           
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(cors());
+app.use(bodyParser.json());
+
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+client.connect();
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => 
+  {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+app.post('/api/login', async (req, res, next) => 
+{
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+    
+ var error = '';
+
+  const { login, password } = req.body;
+
+  const db = client.db();
+  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+
+  var id = -1;
+  var fn = '';
+  var ln = '';
+
+  if( results.length > 0 )
+  {
+    id = results[0].UserId;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
+  }
+
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+  res.status(200).json(ret);
+});
